e55403ed3471e86981a08ec143d081a8d99f495f
Tony <awalker780@gmail.com>
2021-11-18 17:51:20 -0500

    Added comments

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,16 @@
+// Function takes in a URL and a file path as command line arguments and downloads the body of the URL to the file.
+
 const request = require("request");
 const argv = process.argv.slice(2);
 const fs = require("fs");
 
 request(argv[0], (error, response, body) => {
 
-  if (error) {
-    console.log("Error: ", error);
-  }
-
   console.log("Status Code: ", response.statusCode);
+
+  if (error) {
+  console.log("Error: ", error);
+  };
 
   fs.appendFile(argv[1], body, err => {
     if (err) {
